39fa7d32f0b37cdeb4a0c3ba71415b77b4631a13
kalai21797 <67307084+kalai21797@users.noreply.github.com>
2020-07-10 22:08:10 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="container";
+
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+if(!$conn){
+	echo("not connected".mysqli_connect_error());
+}
+?>
