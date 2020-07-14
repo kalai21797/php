c040b465261099b4635231cbe92b53e78ee196a3
kalai21797 <67307084+kalai21797@users.noreply.github.com>
2020-07-14 19:28:58 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,5 +1,68 @@
 <html>
 <body>
-<center><h3>LOGIN CREATED SUCCESSFULLY!!</h3></center>
+
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="todo";
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+if(!$conn){
+	echo"not connected".mysqli_connect_error($conn);
+}
+?>
+
+<?php
+$nameErr="";
+$idErr="";
+if(isset($_POST['add'])){
+	$name=$_POST['name'];
+	$id=$_POST['id'];
+	$sql="INSERT INTO fruits(id,fname) VALUES('$id','$name')";
+	if(mysqli_query($conn,$sql)){
+	echo"inserted";
+	}
+}
+if(isset($_POST['del'])){
+	$name=$_POST['name'];
+	$id=$_POST['id'];
+	$sql="DELETE FROM fruits WHERE id=$id";
+	if(mysqli_query($conn,$sql)){
+	echo"deleted";
+	}
+}
+if(isset($_POST['edit'])){
+	$name=$_POST['name'];
+	$id=$_POST['id'];
+	$sql="UPDATE fruits SET fname='$name' WHERE id='$id' ";
+	if(mysqli_query($conn,$sql)){
+	echo"edited";
+	}
+}
+$names=mysqli_query($conn,"SELECT * FROM fruits");
+?>
+
+<center><h3>LOGGED IN SUCCESSFULLY!!</h3>
+<form method="post" action="<?php $_SERVER["PHP_SELF"] ?>">
+<table>
+<tr>
+<th>Id</th>
+<th>Fruits Name</th>
+</tr>
+<tr>
+<td><input type="text" name="id" required></td>
+<td><input type="text" name="name" required></td>
+<td><input type="submit" name="add" value="add">
+<td><input type="submit" name="del" value="delete"></td>
+<td><input type="submit" name="edit" value="edit"></td>
+</tr>
+
+<?php while($row=mysqli_fetch_array($names)){ ?>
+<tr>
+<td><?php echo $row['id'];  ?></td>
+<td><?php echo $row['fname']; ?></td>
+</tr>
+<?php } ?>
+</center>
 </body>
 </html>
