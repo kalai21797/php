3e26f43caf24e29f1f786f77899d1889fd540cad
kalai21797 <67307084+kalai21797@users.noreply.github.com>
2020-07-23 15:24:06 +0530

    Update ex1.php

--- ex1.php
+++ ex1.php
@@ -1,66 +1,12 @@
 <?php
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="todo";
-$conn=mysqli_connect($servername,$username,$password,$dbname);
-if(!$conn){
-	echo"not connected".mysqli_connect_error($conn);
-}
+include "con2.php";
 ?>
-
-<?php
-if(!empty($_POST['data'])){
-$form = $_POST['data'];
-foreach($form as $data){
-	$form_data[$data['name']] =$data['value'];
-}
-	echo "<pre>";print_r($form_data);echo"</pre>";die();
-}
-
-if(isset($_POST['add'])){
-	$name=$_POST['name'];
-	$id=$_POST['id'];
-	$sql="INSERT INTO fruits(id,fname) VALUES('$id','$name')";
-	if(mysqli_query($conn,$sql)){
-	echo"inserted";
-	}
-}
-if(isset($_POST['del'])){
-	$name=$_POST['name'];
-	$id=$_POST['id'];
-	$sql="DELETE FROM fruits WHERE id=$id";
-	if(mysqli_query($conn,$sql)){
-	echo"deleted";
-	}
-}
-if(isset($_POST['edit'])){
-	$name=$_POST['name'];
-	$id=$_POST['id'];
-	$sql="UPDATE fruits SET fname='$name' WHERE id='$id' ";
-	if(mysqli_query($conn,$sql)){
-	echo"edited";
-	}
-}
-$names=mysqli_query($conn,"SELECT * FROM fruits");
-?>
-<?php while($row=mysqli_fetch_array($names)){ ?>
-<tr>
-<td><?php echo $row['id'];  ?></td>
-<td><?php echo $row['fname']; ?></td>
-</tr>
-<?php } ?>
-
 <html>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 <body>
-<center><h3>FRUITS LIST</h3>
 <form method="post" id="frm" action= "<?php echo $_SERVER['PHP_SELF']?>" >
+<center>
 <table>
-<tr>
-<th>Id</th>
-<th>Fruits Name</th>
-</tr>
 <tr>
 <td><input type="text" name="id" id="id" required></td>
 <td><input type="text" name="name" id="name" required></td>
@@ -73,32 +19,20 @@ $names=mysqli_query($conn,"SELECT * FROM fruits");
 
 <script>
 $(document).ready(function(){
-$('#add').click(function(){
-	var form_data = $('#frm').serializeArray();
-	$.ajax({
-	url:"ex1.php",
-	type:"POST",
-	data:{data:form_data},
-	success:function(response){
-    console.log(response);
-    }
+	$('#add').click(function(){
+		var form_data = $('#frm').serializeArray();
+		//console.log(form_data);
+		//return false;
+		$.ajax({
+			url:"example.php",
+			type:"POST",
+			data:{data:form_data},
+			success:function(response){
+				console.log(response);
+			}
+		});
+		return false;
 	});
-	return false;
 });
-$('#del').click(function(){
-	var form_data = $('#frm').serializeArray();
-	$.ajax({
-	url:"ex1.php",
-	type:"POST",
-	data:{data:form_data},
-	success:function(response){
-    console.log(response);
-    }
-	});
-	return false;
-});
-});
-
-
-
 </script>
+
